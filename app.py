f8c6f67c4ebe288fac8cbfd1b44e1c02e7f8b02c
aaaafly <38147240+aaaafly@users.noreply.github.com>
2019-08-29 15:55:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 SECRET = os.environ.get('SECRET')
+group_id = os.environ.get('group_id')
 
 line_bot_api = LineBotApi(ACCESS_TOKEN)
 handler = WebhookHandler(SECRET)
@@ -42,8 +43,13 @@ def handle_message(event):
 
 #帕妮妮?
 #------------------------------------------------------------------------------------------------------#
-    if(get == 'test'):
-        msg = TextSendMessage('test' )
+    if(get == 'get'):
+        msg = TextSendMessage('get')
+
+        member_ids_res = line_bot_api.get_group_member_ids(group_id)
+        print(member_ids_res.member_ids)
+        print(member_ids_res.next)
+
         #回復訊息msg
         line_bot_api.reply_message(event.reply_token,msg)        
 # -----------------------------------------------------------------------------------------------------#
